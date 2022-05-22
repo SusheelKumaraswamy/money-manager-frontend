e07b263b66d0087aad595ddc91ecfebcebec7b7f
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2022-05-22 19:17:22 +0530

    Update personal.js

--- src/personal.js
+++ src/personal.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useState} from "react";
 import axios from "axios";
-export default function personal(){
+export default function Personal(){
     const [input, setInput] = useState({
         user: []
       });
